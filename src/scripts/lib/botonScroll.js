5d0d5ba8efd3645789e37625c28d314e6b4e9e76
Alex Ballera <alexballera@gmail.com>
2016-12-02 00:03:18 -0400

    Optimizando codigo

--- src/scripts/lib/botonScroll.js
+++ src/scripts/lib/botonScroll.js
@@ -2,7 +2,7 @@ import $ from 'jquery'
 
 export default function () {
   // Con el siguiente código vemos la posicion en el title
-  // $(document).scroll(function(e){
+  // $(document).scroll(function (e) {
   //   document.title = $(document).scrollTop()
   // })
 
@@ -13,27 +13,27 @@ export default function () {
 
   $('.gotoconsultorio').click(function (e) {
     e.preventDefault()
-    $('body,html').animate({scrollTop: '7371'}, 500)
+    $('body,html').animate({scrollTop: '7301'}, 500)
   })
 
   $('.gotomedicas').click(function (e) {
     e.preventDefault()
-    $('body,html').animate({scrollTop: '7949'}, 500)
+    $('body,html').animate({scrollTop: '7839'}, 500)
   })
 
   $('.gotocirugias').click(function (e) {
     e.preventDefault()
-    $('body,html').animate({scrollTop: '8460'}, 500)
+    $('body,html').animate({scrollTop: '8378'}, 500)
   })
 
   $('.gotohotel').click(function (e) {
     e.preventDefault()
-    $('body,html').animate({scrollTop: '9021'}, 500)
+    $('body,html').animate({scrollTop: '8934'}, 500)
   })
 
   $('.gotocontactanos').click(function (e) {
     e.preventDefault()
-    $('body,html').animate({scrollTop: '9614'}, 500)
+    $('body,html').animate({scrollTop: '9538'}, 500)
   })
 
   $('.gotolocalizacion').click(function (e) {
