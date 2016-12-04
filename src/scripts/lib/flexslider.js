1e7b46002b622250a6364ad58c3f047c8a8c6177
Alex Ballera <alexballera@gmail.com>
2016-12-03 22:27:59 -0400

    Terminado Footer

--- src/scripts/lib/flexslider.js
+++ src/scripts/lib/flexslider.js
@@ -5,16 +5,16 @@ export default function () {
   $('.flexslider__principal').flexslider({
     controlNav: false,
     animation: 'slide',
-    pauseOnHover: true,
+    // pauseOnHover: true,
     maxItems: 6,
     prevText: '',
     nextText: ''
   })
   $('.flexslider__mini').flexslider({
     animation: 'slide',
-    // animationLoop: false,
+    slideshowSpeed: 5000,
+    animationSpeed: 700,
     itemWidth: 210,
-    // itemMargin: 3,
     minItems: 3,
     maxItems: 3,
     controlNav: false,
