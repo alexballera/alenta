686337419d03810fe9424365f9c43e6b81e4b4e5
Alex Ballera <alexballera@gmail.com>
2016-12-03 20:34:13 -0400

    Mejorado footer slider

--- src/scripts/lib/flexslider.js
+++ src/scripts/lib/flexslider.js
@@ -4,10 +4,21 @@ import flexslider from '../vendors/jquery.flexslider.js'
 export default function () {
   $('.flexslider__principal').flexslider({
     controlNav: false,
-    directionNav: true,
     animation: 'slide',
     pauseOnHover: true,
     maxItems: 6,
+    prevText: '',
+    nextText: ''
+  })
+  $('.flexslider__mini').flexslider({
+    animation: 'slide',
+    // animationLoop: false,
+    itemWidth: 210,
+    // itemMargin: 3,
+    minItems: 3,
+    maxItems: 3,
+    controlNav: false,
+    directionNav: false,
     prevText: '',
     nextText: ''
   })
