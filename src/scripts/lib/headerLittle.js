cf66dd1c569388cb913e5a2c567e6aa82d8a3fd1
Alex Ballera <alexballera@gmail.com>
2016-12-04 11:36:23 -0400

    Mejorando codigo

--- src/scripts/lib/headerLittle.js
+++ src/scripts/lib/headerLittle.js
@@ -1,6 +1,5 @@
 export default function () {
   var navbar = document.getElementById('header')
-  var secondMenu = document.getElementById('dropdown__secondlevel')
 
   window.onscroll = () => {
     doHeaderLittle()
@@ -9,10 +8,8 @@ export default function () {
   function doHeaderLittle () {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
       navbar.classList.add('header--little')
-      // secondMenu.style.marginTop = 0
     } else {
       navbar.classList.remove('header--little')
-      // secondMenu.style.marginTop = '42px'
     }
   }
 }
