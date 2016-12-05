9efd0396c48573d53e674b944cc2d4cf963bed7c
Alex Ballera <alexballera@gmail.com>
2016-12-05 01:59:01 -0400

    Agregada animacion a menu flotante

--- src/scripts/lib/addNavbarFloating.js
+++ src/scripts/lib/addNavbarFloating.js
@@ -1,19 +1,13 @@
 import $ from 'jquery'
 
 export default function () {
-  var template = `
-      <!-- Menu Flotante -->
-              <nav class="menu__flotante">
-                    <ul class="menu__flotante__contenedor">
-                          <li class="menu__flotante__item"><a href="#alenta"><span>Qué es Alenta</span></a></li>
-                          <li class="menu__flotante__item"><a href="#localizacion"><span>Ubicación</span></a></li>
-                          <li class="menu__flotante__item"><a href="#proyecto"><span>Proyecto</span></a></li>
-                          <li class="menu__flotante__item"><a href="#etapa1"><span>Etapa 1</span></a></li>
-                          <li class="menu__flotante__item"><a href="#etapa2"><span>Etapa 2</span></a></li>
-                          <li class="menu__flotante__item"><a href="#etapa3"><span>Etapa 3</span></a></li>
-                          <li class="menu__flotante__item"><a href="#promotores"><span>Promotores</span></a></li>
-                          <li class="menu__flotante__item"><a href="#contactenos"><span>Contáctenos</span></a></li>
-                    </ul>
-              </nav>`
-  $('.seccion__alenta').prepend(template)
+  $('.menu__flotante').hide()
+  $(document).on('scroll', function () {
+    var desplazamiento = $(document).scrollTop()
+    if (desplazamiento > 600) {
+      $('.menu__flotante').show('slow')
+    } else {
+      $('.menu__flotante').hide('slow')
+    }
+  })
 }
