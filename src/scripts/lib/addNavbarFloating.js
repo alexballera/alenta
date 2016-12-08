cf93650cb2e627996b5d5966209d73757fb21622
Alex Ballera <alexballera@gmail.com>
2016-12-08 15:37:58 -0400

    Mejorado menu flotante

--- src/scripts/lib/addNavbarFloating.js
+++ src/scripts/lib/addNavbarFloating.js
@@ -1,4 +1,6 @@
 import $ from 'jquery'
+import queryUi from '../vendors/jquery-ui.js'
+
 var documento = $(document)
 var menu_flotante = documento.find('.menu__flotante')
 var menu_flotante_contenedor = documento.find('.menu__flotante__contenedor')
@@ -7,19 +9,19 @@ var hide_menu = menu_flotante.find('#menu_flotante_hide')
 
 export default function () {
   var hideMenu = function () {
-    menu_flotante_contenedor.hide('slow')
-    hide_menu.hide('slow')
-    show_menu.show('slow')
+    menu_flotante_contenedor.hide('slide', {direction: 'left'}, 1000)
+    hide_menu.hide('slide', {direction: 'left'}, 1000)
+    show_menu.show('slide', {direction: 'left'}, 1000)
   }
 
   var showMenu = function () {
-    menu_flotante_contenedor.show('slow')
-    hide_menu.show('slow')
-    show_menu.hide('slow')
+    menu_flotante_contenedor.show('slide', {direction: 'left'}, 1000)
+    hide_menu.show('slide', {direction: 'left'}, 1000)
+    show_menu.hide('slide', {direction: 'left'}, 1000)
   }
 
-  menu_flotante_contenedor.hide('slow')
-  hide_menu.hide('slow')
+  menu_flotante_contenedor.hide()
+  hide_menu.hide()
 
   documento.on('scroll', function () {
     var desplazamiento = documento.scrollTop()
