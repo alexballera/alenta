3a76408ad9efe7b172ba66a37daf3b308aeaf652
Alex Ballera <alexballera@gmail.com>
2016-12-10 18:11:54 -0400

    Agregado boton to top

--- src/scripts/lib/botonScroll.js
+++ src/scripts/lib/botonScroll.js
@@ -1,4 +1,7 @@
 import $ from 'jquery'
+var documento = $(document)
+var gototop = documento.find('.gototop')
+var boton__up = documento.find('#boton__up')
 
 export default () => {
   // Con el siguiente código vemos la posicion en el title
@@ -6,8 +9,19 @@ export default () => {
   //   document.title = $(document).scrollTop()
   // })
 
-  $('.gototop').click(function (e) {
+  gototop.click(function (e) {
     e.preventDefault()
     $('body,html').animate({scrollTop: '0'}, 1500)
+  })
+
+  boton__up.hide()
+
+  documento.on('scroll', () => {
+    var desplazamiento = documento.scrollTop()
+    if (desplazamiento >= 2800) {
+      boton__up.show('slow')
+    } else {
+      boton__up.hide('slow')
+    }
   })
 }
