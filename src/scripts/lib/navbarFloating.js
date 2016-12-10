0423610f95d9ea550bb66d770bc89157082a6116
Alex Ballera <alexballera@gmail.com>
2016-12-09 22:11:10 -0400

    Mejorado menu flotante

--- src/scripts/lib/navbarFloating.js
+++ src/scripts/lib/navbarFloating.js
@@ -9,36 +9,46 @@ var hide_menu = menu_flotante.find('#menu_flotante_hide')
 
 export default () => {
   var hideMenu = () => {
-    menu_flotante_contenedor.hide('slide', {direction: 'left'}, 1000)
-    hide_menu.hide('slide', {direction: 'left'}, 1000)
-    show_menu.show('slide', {direction: 'left'}, 1000)
+    menu_flotante_contenedor.hide('slide', {direction: 'left'}, 700)
+    hide_menu.hide('slide', {direction: 'left'}, 700)
   }
 
   var showMenu = () => {
-    menu_flotante_contenedor.show('slide', {direction: 'left'}, 1000)
-    hide_menu.show('slide', {direction: 'left'}, 1000)
-    show_menu.hide('slide', {direction: 'left'}, 1000)
+    menu_flotante_contenedor.show('slide', {direction: 'left'}, 700)
+    hide_menu.show('slide', {direction: 'left'}, 700)
   }
 
   menu_flotante_contenedor.hide()
   hide_menu.hide()
+  show_menu.hide()
 
   documento.on('scroll', () => {
     var desplazamiento = documento.scrollTop()
-    if (desplazamiento >= 600 && desplazamiento <= 2000) {
+    if (desplazamiento >= 600 && desplazamiento <= 2400) {
       showMenu()
     } else {
       hideMenu()
     }
   })
 
+  documento.on('scroll', () => {
+    var desplazamiento = documento.scrollTop()
+    if (desplazamiento > 2400) {
+      show_menu.show()
+    } else {
+      show_menu.hide()
+    }
+  })
+
   hide_menu.click(function (e) {
     e.preventDefault()
     hideMenu()
+    show_menu.show('slide', {direction: 'left'}, 700)
   })
 
   show_menu.click(function (e) {
     e.preventDefault()
     showMenu()
+    show_menu.hide('slide', {direction: 'left'}, 700)
   })
 }
