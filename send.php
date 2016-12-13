9f41ac291863ccd3f5d004307995feb22a0a4932
Alex Ballera <alexballera@gmail.com>
2016-12-13 12:21:23 -0400

    Update 2016-12-13T16:23:38.340Z

--- send.php
+++ send.php
@@ -2,29 +2,34 @@
 
 
 if($_POST){
-  $nombre = strip_tags($_POST['name']);
+  $nombre = strip_tags($_POST['form_name']);
   $fecha = time();
   $fechaFormateada = date("j/n/Y", $fecha);
-  $de = $_POST["name"];
+  $de = $_POST["form_name"];
+  $name = $_POST["form_name"];
+  $lastname = $_POST["form_lastname"];
+  $email = $_POST["form_email"];
+  $telephone: $_POST["form_phone"];
+  $message = $_POST["form_msg"];
   $destino = "alexballera@gmail.com";
   $asunto = "Tienes un mensaje de $nombre ";
   $mensaje .= "Mensaje Desde El Formulario Web Alenta."."\n";
   $mensaje .= "\n";
-  $mensaje .= "Nombre: " . utf8_decode($_POST["name"]) ."\n";
-  $mensaje .= "\n";
-  $mensaje .= "Correo: " . utf8_decode($_POST["email"]) ."\n";
-  $mensaje .= "\n";
-  $mensaje .= "Teléfono: " . utf8_decode($_POST["telephone"]) ."\n";
+  // $mensaje .= "Nombre: $nombre \n";
+  // $mensaje .= "\n";
+  // $mensaje .= "Correo: $email \n";
+  // $mensaje .= "\n";
+  // $mensaje .= "Teléfono: $telephone \n";
+  // $mensaje .= "\n";
+  // $mensaje .= "Mensaje: $message \n";
   $mensaje .= "\n";
   $mensaje .= "Fecha: " . $fechaFormateada ."\n";
-  $mensaje .= "\n";
-  $mensaje .= "Mensaje: " . utf8_decode($_POST["message"]) ."\n";
   $header = "From: alexballera@gmail.com\r\n";
   $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
   $header .= "Mime-Version: 1.0 \r\n";
   $header .= "Content-Type: text/plain; charset=iso-8859-1" . " \r\n";
     // $emailheader = "From: alexballera@gmail.com\r\n";
-mail($destino, $asunto, $mensaje, $header) or die ("Lo sentimos, tu solicitud no ha sido enviada.<br/>Intentelo de nuevo.");
+mail($destino, $asunto, $mensaje, $header);
 // echo utf8_decode(utf8_encode('Tu consulta ha sido enviada correctamente.'));
 // echo '<script language="javascript">alert("Tu consulta ha sido enviada correctamente.");</script>';
   // } else {
