4baa8ba23ad9018f79bbd5ff76974845b2c74a5e
Alex Ballera <alexballera@gmail.com>
2016-12-13 10:45:43 -0400

    Update 2016-12-13T14:46:32.339Z

--- send.php
+++ send.php
@@ -1,10 +1,10 @@
 <?php
 
-$nombre = strip_tags($_POST['name']);
-$fecha = time();
-$fechaFormateada = date("j/n/Y", $fecha);
 
-if($_POST["name"] && $_POST["email"] != ""){
+if($_POST){
+  $nombre = strip_tags($_POST['name']);
+  $fecha = time();
+  $fechaFormateada = date("j/n/Y", $fecha);
   $de = $_POST["name"];
   $destino = "alexballera@gmail.com";
   $asunto = "Tienes un mensaje de $nombre ";
@@ -22,16 +22,20 @@ if($_POST["name"] && $_POST["email"] != ""){
   $header = "From: alexballera@gmail.com\r\n";
   $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
   $header .= "Mime-Version: 1.0 \r\n";
-  $header .= "Content-Type: text/plain";
+  $header .= "Content-Type: text/plain; charset=iso-8859-1" . " \r\n";
     // $emailheader = "From: alexballera@gmail.com\r\n";
 mail($destino, $asunto, $mensaje, $header) or die ("Lo sentimos, tu solicitud no ha sido enviada.<br/>Intentelo de nuevo.");
-echo utf8_decode(utf8_encode('Tu consulta ha sido enviada correctamente.'));
-  } else {
-    if($_POST["name"] == ""){
-    echo utf8_encode ('Por favor, indica tu nombre.');
-    exit; }
-    if($_POST["email"] == ""){
-    echo utf8_encode ('Por favor, indica un email de contacto.');
-    exit; }
+// echo utf8_decode(utf8_encode('Tu consulta ha sido enviada correctamente.'));
+// echo '<script language="javascript">alert("Tu consulta ha sido enviada correctamente.");</script>';
+  // } else {
+//
+    // if($_POST["name"] == ""){
+    // echo utf8_encode ('Por favor, indica tu nombre.');
+    // echo '<script language="javascript">alert("Por favor, indica tu nombre.");</script>';
+    // exit; }
+    // if($_POST["email"] == ""){
+    // echo utf8_encode ('Por favor, indica un email de contacto.');
+    // echo '<script language="javascript">alert("Por favor, indica un email de contacto.");</script>';
+    // exit; }
 }
 ?>
