7c8eeef477e626e5ce6087bd981cd8bdf4178f9d
Alex Ballera <alexballera@gmail.com>
2017-02-28 10:25:04 -0400

    mejorado background de formularios slide

--- src/scripts/lib/formAlenta.js
+++ src/scripts/lib/formAlenta.js
@@ -1,5 +1,5 @@
 import $ from 'jquery'
-import queryUi from '../vendors/jquery-ui.js'
+import '../vendors/jquery-ui.js'
 
 var formSection = `
 <form name="formulario" class="formulario__section" id="formularioAlenta" action="sendMail.php" method="post" >
@@ -34,88 +34,78 @@ var formSection = `
 export default () => {
   $('#formulario_alenta').append(formSection)
 
-  $('#formularioAlenta').submit(function(event) {
-
-    $('.form-group').removeClass('has-error'); // remove the error class
-    $('.help-block').remove(); // remove the error text
+  $('#formularioAlenta').submit(function (event) {
+    $('.form-group').removeClass('has-error') // remove the error class
+    $('.help-block').remove() // remove the error text
 
         // get the form data
         // there are many ways to get this data using jQuery (you can use the class or id also)
-        var formData = {
-            'name'      : $('input#nameAlenta').val(),
-            'email'     : $('input#emailAlenta').val(),
-            'phone'     : $('input#phoneAlenta').val(),
-            'msg'       : $('textarea#msgAlenta').val()
-        };
+    var formData = {
+      'name': $('input#nameAlenta').val(),
+      'email': $('input#emailAlenta').val(),
+      'phone': $('input#phoneAlenta').val(),
+      'msg': $('textarea#msgAlenta').val()
+    }
 
         // process the form
-        $.ajax({
-            type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-            url         : 'sendMail.php', // the url where we want to POST
-            data        : formData, // our data object
-            dataType    : 'json', // what type of data do we expect back from the server
-            encode      : true
-        })
+    $.ajax({
+      type: 'POST', // define the type of HTTP verb we want to use (POST for our form)
+      url: 'sendMail.php', // the url where we want to POST
+      data: formData, // our data object
+      dataType: 'json', // what type of data do we expect back from the server
+      encode: true
+    })
             // using the done promise callback
-            .done(function(data) {
-
+            .done(function (data) {
                 // log data to the console so we can see
-                console.log(data);
+              console.log(data)
 
                 // here we will handle errors and validation messages
-                if ( ! data.success) {
-
+              if (!data.success) {
                   // handle errors for name ---------------
-                  if (data.errors.name) {
-                      $('#nameAlenta-group').addClass('has-error'); // add the error class to show red input
-                      $('#nameAlenta-group').append('<div class="help-block">' + data.errors.name + '</div>'); // add the actual error message under our input
-                  }
+                if (data.errors.name) {
+                  $('#nameAlenta-group').addClass('has-error') // add the error class to show red input
+                  $('#nameAlenta-group').append('<div class="help-block">' + data.errors.name + '</div>') // add the actual error message under our input
+                }
 
                   // handle errors for email ---------------
-                  if (data.errors.email) {
-                      $('#emailAlenta-group').addClass('has-error'); // add the error class to show red input
-                      $('#emailAlenta-group').append('<div class="help-block">' + data.errors.email + '</div>'); // add the actual error message under our input
-                  }
-
-                } else {
-
+                if (data.errors.email) {
+                  $('#emailAlenta-group').addClass('has-error') // add the error class to show red input
+                  $('#emailAlenta-group').append('<div class="help-block">' + data.errors.email + '</div>') // add the actual error message under our input
+                }
+              } else {
                   // ALL GOOD! just show the success message!
                   // $('form').append('<div class="alert alert-success">' + data.message + '</div>');
 
                   // usually after form submission, you'll want to redirect
                   // window.location = '/thank-you'; // redirect a user to another page
-                  console.log(data.message)
-                  $( "#dialog" ).dialog({
-                    show: {
-                      effect: "explode",
-                      duration: 700
-                    },
-                    hide: {
-                      effect: "explode",
-                      duration: 700
-                    }
-                  });
-                  $('input#nameAlenta').val('')
-                  $('input#lastnameAlenta').val('')
-                  $('input#emailAlenta').val('')
-                  $('input#phoneAlenta').val('')
-                  $('textarea#msgAlenta').val('')
-
+                console.log(data.message)
+                $('#dialog').dialog({
+                  show: {
+                    effect: 'explode',
+                    duration: 700
+                  },
+                  hide: {
+                    effect: 'explode',
+                    duration: 700
+                  }
+                })
+                $('input#nameAlenta').val('')
+                $('input#lastnameAlenta').val('')
+                $('input#emailAlenta').val('')
+                $('input#phoneAlenta').val('')
+                $('textarea#msgAlenta').val('')
               }
-
-
             })
 
             // using the fail promise callback
-            .fail(function(data) {
-
+            .fail(function (data) {
                 // show any errors
                 // best to remove for production
-                console.log(data);
-            });
+              console.log(data)
+            })
 
         // stop the form from submitting the normal way and refreshing the page
-        event.preventDefault();
-    });
-
+    event.preventDefault()
+  })
 }
