6c1f943047195f32aa9329d63e40350f8cd573a6
Alex Ballera <alexballera@gmail.com>
2017-04-01 09:27:02 -0400

    Animacion a TopNavBar

--- src/components/topnavbar/TopNavBar.jsx
+++ src/components/topnavbar/TopNavBar.jsx
@@ -3,16 +3,24 @@
 import React from 'react'
 import {Navbar, NavItem} from 'react-materialize'
 import Dropdown from './Dropdown.jsx'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 export default () => {
   return (
-    <Navbar brand='Alenta' right className='navbar-top'>
-      <NavItem><Dropdown item='Alenta' /></NavItem>
-      <NavItem href=''>Consultorios</NavItem>
-      <NavItem href=''>Áreas Médicas</NavItem>
-      <NavItem href=''>Salas De Cirugía</NavItem>
-      <NavItem href=''>Hotel</NavItem>
-      <NavItem href=''>Contáctenos</NavItem>
-    </Navbar>
+    <ReactCSSTransitionGroup
+      transitionName='anim'
+      transitionAppear
+      transitionAppearTimeout={5000}
+      transitionEnter={false}
+      transitionLeave={false}>
+      <Navbar brand='Alenta' right className='navbar-top'>
+        <NavItem><Dropdown item='Alenta' /></NavItem>
+        <NavItem href=''>Consultorios</NavItem>
+        <NavItem href=''>Áreas Médicas</NavItem>
+        <NavItem href=''>Salas De Cirugía</NavItem>
+        <NavItem href=''>Hotel</NavItem>
+        <NavItem href=''>Contáctenos</NavItem>
+      </Navbar>
+    </ReactCSSTransitionGroup>
   )
 }
