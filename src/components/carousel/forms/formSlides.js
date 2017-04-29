f671c0c3a9e76c0d1a2e4b7add6fbd3c786681c4
Alex Ballera <alexballera@gmail.com>
2017-04-29 17:31:49 -0400

    formulario

--- src/components/carousel/forms/formSlides.js
+++ src/components/carousel/forms/formSlides.js
@@ -2,47 +2,47 @@
 
 const Data = {
   formSlide1: {
-    first_name: 'first_name1',
-    last_name: 'last_name1',
-    email: 'email1',
-    telephone: 'telephone1',
-    textarea1: 'textarea1'
+    name: 'name',
+    lastname: 'lastname',
+    email: 'email',
+    phone: 'phone',
+    textarea1: 'msg'
   },
   formSlide2: {
-    first_name: 'first_name2',
-    last_name: 'last_name2',
+    name: 'name2',
+    lastname: 'lastname2',
     email: 'email2',
-    telephone: 'telephone2',
-    textarea1: 'textarea2'
+    phone: 'phone2',
+    textarea1: 'msg2'
   }
 }
 
 const formSlide1 = `
 <div class="row formulario-slide">
-  <form class="col s12">
+  <form name="formulario" class="col s12" id="formularioSlide1">
     <div class="row">
-      <div class="input-field col s6">
-        <input id=${Data.formSlide1.first_name} type="text" >
-        <label for=${Data.formSlide1.first_name}>Nombre</label>
+      <div id="name-group" class="input-field col s6">
+        <input id=${Data.formSlide1.name} name=${Data.formSlide1.name} type="text" >
+        <label for=${Data.formSlide1.name}>Nombre</label>
       </div>
-      <div class="input-field col s6">
-        <input id=${Data.formSlide1.last_name} type="text" >
-        <label for=${Data.formSlide1.last_name}>Apellido</label>
+      <div id="lastname-group" class="input-field col s6">
+        <input id=${Data.formSlide1.lastname} name=${Data.formSlide1.lastname} type="text" >
+        <label for=${Data.formSlide1.lastname}>Apellido</label>
       </div>
     </div>
     <div class="row">
-      <div class="input-field col s6">
-        <input id=${Data.formSlide1.email} type="email" >
+      <div id="email-group" class="input-field col s6">
+        <input id=${Data.formSlide1.email} name=${Data.formSlide1.email} type="email" >
         <label for=${Data.formSlide1.email}>Email</label>
       </div>
-      <div class="input-field col s6">
-        <input id=${Data.formSlide1.telephone} type="tel" >
-        <label for=${Data.formSlide1.telephone}>Teléfono</label>
+      <div id="phone-group" class="input-field col s6">
+        <input id=${Data.formSlide1.phone} name=${Data.formSlide1.phone} type="tel" >
+        <label for=${Data.formSlide1.phone}>Teléfono</label>
       </div>
     </div>
     <div class="row">
-      <div class="input-field col s12">
-        <textarea id=${Data.formSlide1.textarea1} class="materialize-textarea"></textarea>
+      <div id="msg-group" class="input-field col s12">
+        <textarea id=${Data.formSlide1.textarea1} name=${Data.formSlide1.textarea1} class="materialize-textarea"></textarea>
         <label for=${Data.formSlide1.textarea1}>Mensaje</label>
       </div>
     </div>
@@ -54,15 +54,15 @@ const formSlide1 = `
 `
 const formSlide2 = `
 <div class="row formulario-slide">
-  <form class="col s12">
+  <form name="formulario" class="col s12" id="formularioSlide2">
     <div class="row">
       <div class="input-field col s6">
-        <input id=${Data.formSlide2.first_name} type="text" >
-        <label for=${Data.formSlide2.first_name}>Nombre</label>
+        <input id=${Data.formSlide2.name} type="text" >
+        <label for=${Data.formSlide2.name}>Nombre</label>
       </div>
       <div class="input-field col s6">
-        <input id=${Data.formSlide2.last_name} type="text" >
-        <label for=${Data.formSlide2.last_name}>Apellido</label>
+        <input id=${Data.formSlide2.lastname} type="text" >
+        <label for=${Data.formSlide2.lastname}>Apellido</label>
       </div>
     </div>
     <div class="row">
@@ -71,8 +71,8 @@ const formSlide2 = `
         <label for=${Data.formSlide2.email}>Email</label>
       </div>
       <div class="input-field col s6">
-        <input id=${Data.formSlide2.telephone} type="tel" >
-        <label for=${Data.formSlide2.telephone}>Teléfono</label>
+        <input id=${Data.formSlide2.phone} type="tel" >
+        <label for=${Data.formSlide2.phone}>Teléfono</label>
       </div>
     </div>
     <div class="row">
--- src/components/carousel/forms/index.js
+++ src/components/carousel/forms/index.js
@@ -1,6 +1,6 @@
 import $ from 'jquery'
 import 'jquery-ui'
-import {formSlide1, formSlide2} from './formSlides'
+import {formSlide1} from './formSlides'
 
 /* <form name="formulario" class="formulario__form flexslider__form flexslider__form__principal form_test" id="formularioSlide" method="post" action="sendMail.php" >
     <div id="name-group" class="formulario__item">
@@ -46,83 +46,85 @@ import {formSlide1, formSlide2} from './formSlides'
 
 export default () => {
   $('.carousel-item > #formSlide1').append(formSlide1)
-  $('.carousel-item > #formSlide2').append(formSlide2)
+  // $('.carousel-item > #formSlide2').append(formSlide2)
 
   $('#dialog').hide()
 
-  $('#formularioSlide').submit(function (event) {
+  $('#formularioSlide1').submit(function (event) {
     $('.form-group').removeClass('has-error') // remove the error class
     $('.help-block').remove() // remove the error text
 
         // get the form data
         // there are many ways to get this data using jQuery (you can use the class or id also)
-    var formData = {
+    const formData = {
       'name': $('input#name').val(),
       'lastname': $('input#lastname').val(),
       'email': $('input#email').val(),
       'phone': $('input#phone').val(),
       'msg': $('textarea#msg').val()
     }
+    // const URLprotocol = window.location.protocol
+    // const URLdomain = window.location.host
+    const url = 'send.php'
+    // console.log(URLprotocol + '//' + URLdomain + '/' + url)
 
-        // process the form
     $.ajax({
-      type: 'POST', // define the type of HTTP verb we want to use (POST for our form)
-      url: 'sendMail.php', // the url where we want to POST
-      data: formData, // our data object
-      dataType: 'json', // what type of data do we expect back from the server
-      encode: true
+      type: 'POST',
+      url: url,
+      data: formData,
+      dataType: 'json',
+      encode: true,
+      statusCode: {
+        404: () => {
+          console.log('page not found')
+        }
+      },
+      success: () => {
+        console.log('Success')
+      }
     })
-            // using the done promise callback
-            .done(function (data) {
-                // log data to the console so we can see
-              console.log(data)
+    .done((data) => {
+      console.log(data)
+      if (!data.success) {
+        if (data.errors.name) {
+          $('#name-group').addClass('has-error')
+          $('#name-group').append('<div class="help-block">' + data.errors.name + '</div>')
+        }
+        if (data.errors.email) {
+          $('#email-group').addClass('has-error')
+          $('#email-group').append('<div class="help-block">' + data.errors.email + '</div>')
+        }
+      } else {
+          // ALL GOOD! just show the success message!
+          // $('form').append('<div class="alert alert-success">' + data.message + '</div>');
 
-                // here we will handle errors and validation messages
-              if (!data.success) {
-                  // handle errors for name ---------------
-                if (data.errors.name) {
-                  $('#name-group').addClass('has-error') // add the error class to show red input
-                  $('#name-group').append('<div class="help-block">' + data.errors.name + '</div>') // add the actual error message under our input
-                }
-
-                  // handle errors for email ---------------
-                if (data.errors.email) {
-                  $('#email-group').addClass('has-error') // add the error class to show red input
-                  $('#email-group').append('<div class="help-block">' + data.errors.email + '</div>') // add the actual error message under our input
-                }
-              } else {
-                  // ALL GOOD! just show the success message!
-                  // $('form').append('<div class="alert alert-success">' + data.message + '</div>');
-
-                  // usually after form submission, you'll want to redirect
-                  // window.location = '/thank-you'; // redirect a user to another page
-                console.log(data.message)
-                $('#dialog').dialog({
-                  show: {
-                    effect: 'explode',
-                    duration: 700
-                  },
-                  hide: {
-                    effect: 'explode',
-                    duration: 700
-                  }
-                })
-                $('input#name').val('')
-                $('input#lastname').val('')
-                $('input#email').val('')
-                $('input#phone').val('')
-                $('textarea#msg').val('')
-              }
-            })
-
-            // using the fail promise callback
-            .fail(function (data) {
-                // show any errors
-                // best to remove for production
-              console.log(data)
-            })
-
-        // stop the form from submitting the normal way and refreshing the page
+          // usually after form submission, you'll want to redirect
+          // window.location = '/thank-you'; // redirect a user to another page
+        console.log(data.message)
+        $('#dialog').dialog({
+          show: {
+            effect: 'explode',
+            duration: 700
+          },
+          hide: {
+            effect: 'explode',
+            duration: 700
+          }
+        })
+        $('input#name').val('')
+        $('input#lastname').val('')
+        $('input#email').val('')
+        $('input#phone').val('')
+        $('textarea#msg').val('')
+      }
+    })
+    .fail((data) => {
+      console.log('Error')
+      console.log(data)
+    })
+    .always(() => {
+      console.log('complete')
+    })
     event.preventDefault()
   })
 }
