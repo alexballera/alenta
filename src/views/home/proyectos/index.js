55eb979def99f77dde9d8673c10bffd0c4426dff
Alex Ballera <alexballera@gmail.com>
2017-04-30 10:49:57 -0400

    terminada etapa 2

--- src/views/home/proyectos/index.js
+++ src/views/home/proyectos/index.js
@@ -2,11 +2,13 @@
 import yo from 'yo-yo'
 import Proyecto from './proyecto'
 import Etapa1 from './etapa1'
+import Etapa2 from './etapa2'
 
 const Proyectos = yo`
 <div>
   ${Proyecto}
   ${Etapa1}
+  ${Etapa2}
 </div>
 `
 module.exports = Proyectos
